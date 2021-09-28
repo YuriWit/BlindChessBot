d331e2b236e97af3c9efe87a63e7e577a2cf7409
YuriWit <yuridewitherran@gmail.com>
2021-09-28 11:56:58 -0300

    added max mistakes

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from PyQt5.QtSvg import QSvgWidget
 from PyQt5.QtWidgets import QApplication, QWidget
 
 engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
+engine.configure({"Skill Level":1})
+engine.configure({"Skill Level":3})
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -18,18 +20,19 @@ class MainWindow(QWidget):
         self.widgetSvg.setGeometry(0, 0, 800, 800)
 
         self.chessboard = chess.Board()
-        self.turn = random.randint(0,1) == 1
+        self.turn = 0 #random.randint(0,1) == 1
 
     def loop(self):
+        strike = 0
         a = os.system('clear')
-        while not self.chessboard.is_game_over():
+        while not self.chessboard.is_game_over() and strike < 5:
             if self.chessboard.turn == self.turn:
                 result = engine.play(self.chessboard, chess.engine.Limit(time=0.1))
                 print(self.chessboard.san(result.move))
                 self.chessboard.push(result.move)
             else:
                 move = ''
-                while move == '':
+                while move == '' and strike < 5:
                     try:
                         move = input("Your move: ")
                         self.chessboard.push_san(move)
@@ -37,11 +40,16 @@ class MainWindow(QWidget):
                         if move == "show":
                             self.see()
                             self.show()
+                        elif move == "cls":
+                            a = os.system('clear')
                         else:
+                            strike += 1
                             print("non valid move")
                         move = ''
                 a = os.system('clear')
         print("game over")
+        print(str(self.chessboard.halfmove_clock)+" moves")
+        self.see()
         self.show()
 
     def see(self):
@@ -52,4 +60,6 @@ if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
     window.loop()
+    input("close")
+    window.close()
     #app.exec()
